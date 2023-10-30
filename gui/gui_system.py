03d63c682430237d86bf31fc1c301e7d2458b157
shuvamshr <shuvamshrestha408@gmail.com>
2023-10-31 09:29:54 +1100

    Updated GUI System

--- gui/gui_system.py
+++ gui/gui_system.py
@@ -1,160 +1,95 @@
 import tkinter as tk
-from utilities.color import *
-from utilities.input_validation import validate_name, validate_email, validate_password
+from tkinter import ttk, messagebox
+import json
+import random
 
 
 class GUISystem:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("University GUI System")
-        self.root.geometry("400x300")
-        self.frame = None
-        self.label = tk.Label(root, text="Welcome to University")
-        self.label.pack()
-        self.student_button = tk.Button(
-            root, text="Student System", command=self.switch_to_student)
-        self.student_button.pack()
-        self.admin_button = tk.Button(
-            root, text="Admin System", command=self.switch_to_admin)
-        self.admin_button.pack()
-        self.admin_access_key = "enr0llm3nt"
-        self.admin_authenticated = False
+    def __init__(self, window):
+        self.window = window
+        self.window.title("Login Application")
 
-    def switch_to_student(self):
-        if self.frame:
-            self.frame.destroy()
-            self.label.config(text="Welcome to University")
-        self.student_button.pack_forget()
-        self.admin_button.pack_forget()
-        self.frame = tk.Frame(self.root)
-        self.frame.pack()
-        login_button = tk.Button(
-            self.frame, text="Login", command=self.login_student)
-        login_button.pack()
-        register_button = tk.Button(
-            self.frame, text="Register", command=self.register_student)
-        register_button.pack()
-        go_back_button = tk.Button(
-            self.frame, text="Go Back", command=self.go_back)
-        go_back_button.pack()
+        with open("student.data", "r") as file:
+            self.student_data = json.load(file)
 
-    def switch_to_admin(self):
-        if self.frame:
-            self.frame.destroy()
-            self.label.config(text="Welcome to University")
-        self.student_button.pack_forget()
-        self.admin_button.pack_forget()
-        self.frame = tk.Frame(self.root)
-        self.frame.pack()
-        if not self.admin_authenticated:
-            admin_special_key_label = tk.Label(
-                self.frame, text="Enter the Admin Access Key:")
-            admin_special_key_label.pack()
-            admin_special_key_entry = tk.Entry(
-                self.frame, show="*")
-            admin_special_key_entry.pack()
-            admin_special_key_submit = tk.Button(
-                self.frame, text="Submit", command=lambda: self.check_special_key(admin_special_key_entry))
-            admin_special_key_submit.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        style = ttk.Style()
+        style.configure("TLabel", font=("Arial", 12))
+        style.configure("TButton", font=("Arial", 12))
+
+        self.frame = ttk.Frame(self.window)
+        self.frame.pack(padx=20, pady=20)
+
+        self.email_label = ttk.Label(self.frame, text="Email:")
+        self.email_label.grid(row=0, column=0, sticky="w", padx=10, pady=5)
+        self.email_entry = ttk.Entry(self.frame)
+        self.email_entry.grid(row=0, column=1, padx=10, pady=5)
+
+        self.password_label = ttk.Label(self.frame, text="Password:")
+        self.password_label.grid(row=1, column=0, sticky="w", padx=10, pady=5)
+        self.password_entry = ttk.Entry(self.frame, show="*")
+        self.password_entry.grid(row=1, column=1, padx=10, pady=5)
+
+        self.login_button = ttk.Button(
+            self.frame, text="Login", command=self.validate_login)
+        self.login_button.grid(row=2, columnspan=2, pady=10)
+
+    def validate_login(self):
+        entered_email = self.email_entry.get()
+        entered_password = self.password_entry.get()
+
+        for student in self.student_data:
+            if student["email"] == entered_email and student["password"] == entered_password:
+                self.open_subjects_window(student)
+                return
+
+        messagebox.showerror("Login Failed", "Invalid email or password")
+
+    def open_subjects_window(self, student):
+        subject_window = tk.Toplevel(self.window)
+        subject_window.title("Subject Codes and Marks")
+        subject_frame = ttk.Frame(subject_window)
+        subject_frame.pack(padx=20, pady=20)
+
+        ttk.Label(subject_frame, text="Subject Code").grid(
+            row=0, column=0, padx=10, pady=5)
+        ttk.Label(subject_frame, text="Marks").grid(
+            row=0, column=1, padx=10, pady=5)
+
+        row = 1
+        for subject_code, marks in student["subject"].items():
+            ttk.Label(subject_frame, text=subject_code).grid(
+                row=row, column=0, padx=10, pady=5)
+            ttk.Label(subject_frame, text=marks).grid(
+                row=row, column=1, padx=10, pady=5)
+            row += 1
+
+        enroll_button = ttk.Button(
+            subject_frame, text="Enroll New Subject", command=lambda: self.enroll_new_subject(student))
+        enroll_button.grid(row=row, columnspan=2, pady=10)
+
+    def enroll_new_subject(self, student):
+        if len(student["subject"]) >= 4:
+            messagebox.showerror(
+                "Error", "You can enroll a maximum of 4 subjects.")
         else:
-            admin_button1 = tk.Button(
-                self.frame, text="View Students", command=self.view_students)
-            admin_button1.pack()
-            admin_button2 = tk.Button(
-                self.frame, text="View Students By Grade", command=self.view_students_grade)
-            admin_button2.pack()
-            admin_button3 = tk.Button(
-                self.frame, text="View Students By PASS/FAIL Status", command=self.view_students_status)
-            admin_button3.pack()
-            admin_button4 = tk.Button(
-                self.frame, text="Remove Student", command=self.remove_student)
-            admin_button4.pack()
-            admin_button5 = tk.Button(
-                self.frame, text="Clear Student Data", command=self.clear_data)
-            admin_button5.pack()
-        go_back_button = tk.Button(
-            self.frame, text="Go Back", command=self.go_back)
-        go_back_button.pack()
+            new_subject_code = str(random.randint(100, 999))
+            new_subject_mark = random.randint(0, 100)
 
-    def switch_to_registration(self):
-        if self.frame:
-            self.frame.destroy()
-            self.label.config(text="Student Registration")
-        self.student_button.pack_forget()
-        self.admin_button.pack_forget()
-        self.frame = tk.Frame(self.root)
-        self.frame.pack()
-        first_name_label = tk.Label(self.frame, text="First Name:")
-        first_name_label.pack()
-        first_name_entry = tk.Entry(self.frame)
-        first_name_entry.pack()
-        last_name_label = tk.Label(self.frame, text="Last Name:")
-        last_name_label.pack()
-        last_name_entry = tk.Entry(self.frame)
-        last_name_entry.pack()
-        email_label = tk.Label(self.frame, text="Email:")
-        email_label.pack()
-        email_entry = tk.Entry(self.frame)
-        email_entry.pack()
-        password_label = tk.Label(self.frame, text="Password:")
-        password_label.pack()
-        password_entry = tk.Entry(self.frame, show="*")
-        password_entry.pack()
-        register_submit_button = tk.Button(self.frame, text="Register", command=lambda: self.register_student(
-            first_name_entry.get(), last_name_entry.get(), email_entry.get(), password_entry.get()))
-        register_submit_button.pack()
-        go_back_button = tk.Button(
-            self.frame, text="Go Back", command=self.go_back)
-        go_back_button.pack()
+            student["subject"][new_subject_code] = new_subject_mark
+            messagebox.showinfo(
+                "New Subject Enrolled", f"Subject {new_subject_code} added with marks {new_subject_mark}")
 
-    def switch_to_student(self):
-        if self.frame:
-            self.frame.destroy()
-            self.label.config(text="Welcome to University")
-        self.student_button.pack_forget()
-        self.admin_button.pack_forget()
-        self.frame = tk.Frame(self.root)
-        self.frame.pack()
-        login_button = tk.Button(
-            self.frame, text="Login", command=self.login_student)
-        login_button.pack()
-        register_button = tk.Button(
-            self.frame, text="Register", command=self.switch_to_registration)
-        register_button.pack()
-        go_back_button = tk.Button(
-            self.frame, text="Go Back", command=self.go_back)
-        go_back_button.pack()
+            with open("student.data", "w") as file:
+                json.dump(self.student_data, file)
 
-    def login_student(self):
-        pass
-
-    def register_student(self, first_name, last_name, email, password):
-        if validate_name(first_name) and validate_name(last_name) and validate_email(email, first_name, last_name) and validate_password(password):
-
-            print(BOLD + GREEN + "Registration successful." + RESET)
-
-        else:
-            print(BOLD + RED + "Registration failed. Please check your input." + RESET)
-
-    def check_special_key(self, admin_special_key_entry):
-        special_key = admin_special_key_entry.get()
-        if special_key == self.admin_access_key:
-            admin_special_key_entry.destroy()
-            self.admin_authenticated = True
-            self.switch_to_admin()
-        else:
-            admin_special_key_entry.delete(0, tk.END)
-
-    def go_back(self):
-        if self.frame:
-            self.frame.destroy()
-            self.label.config(text="Welcome to University")
-        self.admin_authenticated = False
-        self.admin_button.pack()
-        self.student_button.pack()
+            self.open_subjects_window(student)
 
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = GUISystem(root)
-    root.mainloop()
+    app = tk.Tk()
+    system = GUISystem(app)
+    app.geometry("300x200")
+    app.mainloop()
